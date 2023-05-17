a6fbc52e69b2fe591ce8f904f0ff33c46bbc3fec
Vinicitas <sermsriphumrapee@gmail.com>
2023-05-17 12:04:54 +0700

    Ex 4

---
+++ Exercise4_Phumrapee_S.py
@@ -0,0 +1,17 @@
+'''
+F = Foundation English
+G = General Business
+I = Introducing to Computer Systems
+C = Computer Programming
+'''
+
+F = 86
+G = 60.6
+I = 75
+C = 92
+
+print("---Your Score---")
+print("Foundation English :", F)
+print("General Business :", G)
+print("Introducing to Computer Systems :", I)
+print("Computer Programming :", C)
